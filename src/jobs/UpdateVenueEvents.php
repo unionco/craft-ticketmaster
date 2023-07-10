e14c6e09fa63aed8351b220a2a418dda1238d5ab
Abry Rath <abry.rath@union.co>
2023-07-10 14:39:16 -0400

    creating/updating a single entry for performances with multiple dates/times

--- src/jobs/UpdateVenueEvents.php
+++ src/jobs/UpdateVenueEvents.php
@@ -2,42 +2,107 @@
 
 namespace unionco\ticketmaster\jobs;
 
+use craft\helpers\ArrayHelper;
+use DateTime;
+use craft\helpers\Json;
 use craft\queue\BaseJob;
+use Exception;
 use unionco\ticketmaster\Ticketmaster;
+use unionco\ticketmaster\services\LogService;
+use unionco\ticketmaster\services\ElementService;
 use unionco\ticketmaster\models\Venue as VenueModel;
 use unionco\ticketmaster\records\Venue as VenueRecord;
+use unionco\ticketmaster\services\EventService;
 
 class UpdateVenueEvents extends BaseJob
 {
-    public $events;
-    public $venue;
-    public $siteHandle;
+    public array $events = [];
+    /** @var array This will be an array containing the key 'id' */
+    public array $venue = [];
+    public string $siteHandle = 'default';
 
     public function execute($queue): void
     {
-        $eventService = Ticketmaster::$plugin->elements;
-        $count = count($this->events);
-        $venueRecord = VenueRecord::find()->where(['id' => $this->venue])->one();
-        $venueModel = new VenueModel($venueRecord->getAttributes());
+        /** @var ElementService */
+        $elementService = Ticketmaster::$plugin->elements;
+        /** @var EventService */
+        $eventService = Ticketmaster::$plugin->events;
+        /** @var LogService */
+        $log = Ticketmaster::$plugin->log;
 
-        for ($step = 0; $step < $count; ++$step) {
-            $this->setProgress($queue, $step / $count);
+        try {
+            $venue = $this->getVenue();
+            $logPrefix = "[Venue {$venue->title} {$venue->tmVenueId}] ";
 
-            $eventDetails = $this->events[$step];
+            // Before looping, group the events based on their names
+            $groupedEvents = $eventService->groupEventsByName($this->events);
 
-            if ($eventDetails) {
+            foreach ($groupedEvents as $name => $eventInstances) {
+                // Each $eventInstance is the same 'Event', but with a unique performance date/time/URL
+                $instances = [];
+                foreach ($eventInstances as $i => $eventInstance) {
+                    $loop1Index = $i + 1;
+                    $key = "new{$loop1Index}";
+                    $instances[$key] = $eventService->getEventSupertableInfo($eventInstance);
+                }
+                // Events have been grouped, so now proceed with the original plan
+                // Event Instances supertable data will be added to the entry in
+                // `ElementService::saveElement`
+                $eventDetails = ArrayHelper::firstValue($eventInstances);
+                $eventDetails['eventInstances'] = $instances;
                 // save
-                $eventService->saveEvent($eventDetails, $venueModel);
-            } else {
-                return false;
+                $description = $this->getEventDetailsDescription($eventDetails);
+                $log->info("{$logPrefix}Event Details are present, starting save. $description");
+                try {
+                    $elementService->saveEvent($eventDetails, $venue);
+                } catch (\Throwable $e) {
+                    $log->error("{$logPrefix} Error encountered during ElementService::saveEvent.", $e);
+                    throw new Exception("error in saveEvent - {$e->getMessage()}");
+                }
             }
+        } catch (\Throwable $e) {
+            $log->error("Error encountered in UpdateVendueEvents job.", $e);
+            throw $e;
         }
+    }
 
-        return true;
+    public function getVenue(): VenueModel
+    {
+        $venueRecord = VenueRecord::find()->where(['id' => $this->venue])->one();
+        try {
+            $venueModel = new VenueModel($venueRecord->getAttributes());
+            return $venueModel;
+        } catch (\Throwable $e) {
+            /** @todo */
+            throw new Exception("Error at getVenue");
+        }
     }
 
     protected function defaultDescription(): ?string
     {
-        return 'Updating events for venue.';
+        if ($venue = $this->getVenue()) {
+            return "Updating events for venue {$venue->title} [{$venue->tmVenueId}]";
+        }
+        return 'Updating events for venue (details not available)';
+    }
+
+    protected function getEventDetailsDescription(array $eventDetails): string
+    {
+        $desc = "";
+        if ($name = $eventDetails['name'] ?? false) {
+            $desc .= "[Name: $name] ";
+        }
+
+        if ($dateBlock = $eventDetails['dates'] ?? false) {
+            if ($startBlock = $dateBlock['start'] ?? false) {
+                $startDate = new DateTime($startBlock['dateTime']);
+                $formatted = $startDate->format("Y-m-d H:i:s");
+                $desc .= "[Date: $formatted] ";
+            }
+        }
+        if (!$desc) {
+            return "Description not available";
+        }
+        return $desc;
     }
 }
